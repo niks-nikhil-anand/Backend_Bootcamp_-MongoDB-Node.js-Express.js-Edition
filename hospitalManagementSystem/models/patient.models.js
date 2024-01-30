d3cae5f9157209bc625a121868ffdbc26039a653
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-01-30 16:03:43 +0530

    done with the patient data modelling

--- hospitalManagementSystem/models/patient.models.js
+++ hospitalManagementSystem/models/patient.models.js
@@ -1,5 +1,32 @@
 import mongoose from 'mongoose'
 
-const patientSchema = new mongoose.Schema({} ,{timestamp :true})
+const patientSchema = new mongoose.Schema({
+    name : {
+        type : String ,
+        required : true
+    } ,
+    address :{
+        type : String ,
+        required : true
+    },
+    age :{
+        type : Number,
+        required : true
+    },
+    bloodGroup:{
+        type : String ,
+        required : true,
+        enum : ["A", "B", "AB" , "O"]
+    },
+    gender: {
+        type : String ,
+        enum : ["M" , "F" , "O"],
+        required : true
+    },
+    admittedIn :{
+        type : mongoose.Schema.Types.ObjectId ,
+        ref : Hospital
+    }
+} ,{timestamp :true})
 
 export const Patient = mongoose.model("Patient" , patientSchema)
